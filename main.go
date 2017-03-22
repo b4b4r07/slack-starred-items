cc5fa63d7e267363f89672897fd3a90f28a17384
b4b4r07 <b4b4r07@gmail.com>
2017-03-23 00:22:03 +0900

    user.id <-> user.name mapping

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/fatih/color"
 	"github.com/nlopes/slack"
 	"github.com/pelletier/go-toml"
 )
+
+func init() {
+	color.NoColor = false
+}
 
 func main() {
 	config, err := toml.LoadFile("config.toml")
@@ -17,6 +22,16 @@ func main() {
 	userId := config.Get("slack.user_id").(string)
 
 	api := slack.New(apiToken)
+
+	users, err := api.GetUsers()
+	if err != nil {
+		panic(err)
+	}
+	users_map := make(map[string]string, len(users))
+	for _, user := range users {
+		users_map[user.ID] = user.Name
+	}
+	fmt.Printf("%#v\n", users_map)
 
 	// Get all starred items.
 	starParams := slack.NewStarsParameters()
@@ -30,10 +45,19 @@ func main() {
 		}
 		for _, star := range stars {
 			if star.Type == "message" {
-				fmt.Println(star.Message.Msg)
+				if star.Message.Msg.User == "" {
+					continue
+				}
+				user := users_map[star.Message.Msg.User]
+				printStarredMessage(user, star.Message.Msg.Text)
 			}
 		}
 		pages = paging.Pages
 		starParams.Page++
 	}
+}
+
+func printStarredMessage(username, text string) {
+	yellow := color.New(color.FgYellow).SprintFunc()
+	fmt.Printf("%s\t%s\n", yellow("@"+username), text)
 }
